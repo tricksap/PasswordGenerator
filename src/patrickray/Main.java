06770a835730e3bda5e15be6975e7ba6a2e3d3a4
Patrick <tricksap@gmail.com>
2021-01-03 21:52:47 +0800

    Initial commit

---
+++ src/patrickray/Main.java
@@ -0,0 +1,22 @@
+package patrickray;
+
+import java.security.SecureRandom;
+
+public class Main
+{
+    private static final String CHAR_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String CHAR = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMERIC = "0123456789";
+    private static final String SPECIAL_CHARS = "!@#$%^&*_=+-/";
+
+    private static SecureRandom random = new SecureRandom();
+
+    public static void main(String[] args)
+    {
+
+    }
+    public static String generatePassword()
+    {
+
+    }
+}
